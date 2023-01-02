a21fad7d4a2989b2982885f875b048fa6c7090cb
romain <romain@DESKTOP-PDPLBGK>
2023-01-02 16:49:23 +0100

    mon site

---
+++ script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["BTS SIO Option SISR"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["BTS SIO option SISR"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
+nge text every 2 seconds
